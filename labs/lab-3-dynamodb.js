90385daff822f431ae9ac80a292fe8cb0a5fabd4
Chris Atkin <atkinchris@gmail.com>
2016-09-05 15:01:57 +0100

    Add Lab 3 code

--- labs/lab-3-dynamodb.js
+++ labs/lab-3-dynamodb.js
@@ -1,5 +1,55 @@
 const AWS = require('aws-sdk');
+const co = require('co');
 
-const dynamodb = new AWS.DynamoDB();
+AWS.config.update({
+  accessKeyId: 'akid',
+  secretAccessKey: 'secret',
+  region: 'us-west-1',
+});
 
-console.log(dynamodb);
+const INFECTIONS_TABLE = '';
+
+co(function* main() {
+  const dynamodb = new AWS.DynamoDB();
+
+  yield dynamodb.createTable({
+    AttributeDefinitions: [{
+      AttributeName: 'PatientId',
+      AttributeType: 'S',
+    }, {
+      AttributeName: 'City',
+      AttributeType: 'S',
+    }, {
+      AttributeName: 'Date',
+      AttributeType: 'S',
+    }],
+    KeySchema: [{
+      AttributeName: 'PatientId',
+      KeyType: 'HASH',
+    }],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 10,
+    },
+    TableName: INFECTIONS_TABLE,
+    GlobalSecondaryIndexes: [{
+      IndexName: 'CITY_DATE_INDEX_NAME',
+      KeySchema: [{
+        AttributeName: 'City',
+        KeyType: 'HASH',
+      }, {
+        AttributeName: 'Date',
+        KeyType: 'RANGE',
+      }],
+      Projection: {
+        ProjectionType: 'ALL',
+      },
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 10,
+      },
+    }],
+  }).promise();
+
+  console.log('Process complete!');
+}).catch(err => console.error(err));
